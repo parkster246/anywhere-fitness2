c71244de36bf9e9abfe944f1100bea9f847c6d0d
Parker williams <parkster246@gmail.com>
2020-07-30 11:43:19 -0500

    debugging

--- anywhere-fitness-fe/src/store/actions/fitnessActions.js
+++ anywhere-fitness-fe/src/store/actions/fitnessActions.js
@@ -1,32 +1,32 @@
-// import axios from 'axios'
-// import * as act from '../actions'
-// import axiosWithAuth from '../../utils/axiosWithAuth'
+import axios from 'axios'
+import * as act from '../actions'
+import axiosWithAuth from '../../utils/axiosWithAuth'
 
-// export function PostClasses(newClass) {
-//     return (dispatch) => {
-//         dispatch({ type: act.FETCH_INSTRUCTOR_START })
-//         axiosWithAuth().post('/instructor/classes', newClass)
-//             .then(res => {
-//                 dispatch({ type: act.FETCH_INSTRUCTOR_FINISH })
-//                 dispatch({
-//                     type: act.FETCH_INSTRUCTOR_RECEIVED,
-//                     payload: res.data
-//                 })
-//             })
-//             .catch(err => {
-//                 dispatch({ type: act.FETCH_INSTRUCTOR_FINISH })
-//             })
-//     }
-// }
+export function PostClasses(newClass) {
+    return (dispatch) => {
+        dispatch({ type: act.FETCH_INSTRUCTOR_START })
+        axiosWithAuth().post('/instructor/classes', newClass)
+            .then(res => {
+                dispatch({ type: act.FETCH_INSTRUCTOR_FINISH })
+                dispatch({
+                    type: act.FETCH_INSTRUCTOR_RECEIVED,
+                    payload: res.data
+                })
+            })
+            .catch(err => {
+                dispatch({ type: act.FETCH_INSTRUCTOR_FINISH })
+            })
+    }
+}
 
-// export function GetClasses() {
-//     return (dispatch) => {
-//         axiosWithAuth().get('/client/classes/all')
-//     }
-// }
+export function GetClasses() {
+    return (dispatch) => {
+        axiosWithAuth().get('/client/classes/all')
+    }
+}
 
-// export function Login() {
-//     return (dispatch) => {
-//         axiosWithAuth().post('/auth/login', data)
-//     }
-// }
+export function Login() {
+    return (dispatch) => {
+        axiosWithAuth().post('/auth/login', data)
+    }
+}
